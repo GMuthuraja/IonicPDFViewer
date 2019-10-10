86a530696b77f010d48a9fdfd98e2acc15ac33da
Muthuraja.Ganesan <muthuraja.ganesan@raqmiyat.com>
2019-10-10 17:22:31 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { DocumentViewer } from '@ionic-native/document-viewer/ngx';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,9 +19,13 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
+    InAppBrowser,
+    DocumentViewer,
+    File,
+    FileOpener,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer/ngx';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  constructor(private iab: InAppBrowser, private document: DocumentViewer,
+    private fileOpener: FileOpener, private file: File) { }
 
+  openPDFBrowserBlank() {
+    this.iab.create('../../assets/sample.pdf', '_blank');
+  }
+
+  openPDFBrowserSystem() {
+    this.iab.create('../../assets/sample.pdf', '_system');
+  }
+
+  openPDFViewer() {
+    const options: DocumentViewerOptions = {
+      title: 'My PDF'
+    }
+
+    this.document.viewDocument(this.file.applicationDirectory + 'www/assets/sample.pdf', 'application/pdf', options);
+  }
+
+  openPDFOpenerLocal() {
+    this.fileOpener.open(this.file.applicationDirectory + 'www/assets/sample.pdf', 'application/pdf');
+  }
+
+  openPDFOpenerURL() {
+    this.fileOpener.open('http://www.pdf995.com/samples/pdf.pdf', 'application/pdf');
+  }
 }
